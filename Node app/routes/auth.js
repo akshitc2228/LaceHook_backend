3731dbe41039da3e11ecfc0dd234d7824d41b9f8
Akshit Chaturvedi <akshitc2228@gmail.com>
2022-10-10 17:33:05 +0530

    Commit for .gitignore fix

--- Node app/routes/auth.js
+++ Node app/routes/auth.js
@@ -1,16 +1,25 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 //REGISTER
-router.get("/register", async (req, res) => {
-    const user = await new User ({
-        username: "firstUser",
-        email: "firstUser@gmail.com",
-        password: "pass_word"
-    })
+router.post("/register", async (req, res) => {
 
-    await user.save()
-    res.send("CLEAR!")
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt); 
+
+        const newUser = new User ({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        const user = await newUser.save();
+        res.status(200).json(user)
+    } catch (err) {
+        console.log(err)
+    }
 });
 
 module.exports = router
